59c914eefcc54d5435aca3384c8e6aaecc4be959
Minyi Zhu <minyizhu@Minyis-MacBook-Pro.local>
2024-11-24 21:47:31 -0800

    pylint revision

--- app/clients/router.py
+++ app/clients/router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
-from app.clients.service.logic import interpret_and_calculate, create_client_data, get_client_data, update_client_data, delete_client_data
+from app.clients.service.logic import interpret_and_calculate
+from app.clients.service.logic import create_client_data
+from app.clients.service.logic import get_client_data
+from app.clients.service.logic import update_client_data, delete_client_data
 from app.clients.schema import PredictionInput
+
 
 # Define a Pydantic model for client data
 class ClientData(BaseModel):
@@ -40,13 +43,16 @@ class ClientData(BaseModel):
     enhanced_referrals: int
     success_rate: int
 
+
 router = APIRouter(prefix="/clients", tags=["clients"])
+
 
 @router.post("/predictions")
 async def predict(data: PredictionInput):
     print("HERE")
     print(data.model_dump())
     return interpret_and_calculate(data.model_dump())
+
 
 @router.post("/", response_model=ClientData)
 async def create_client(client_data: ClientData):
@@ -68,6 +74,7 @@ async def create_client(client_data: ClientData):
     else:
         raise HTTPException(status_code=400, detail="Unable to create client")
 
+
 @router.get("/", response_model=ClientData)
 async def get_client(age: int, gender: int, work_experience: int):
     """
@@ -82,7 +89,8 @@ async def get_client(age: int, gender: int, work_experience: int):
         ClientData: The client data corresponding to the specified attributes.
 
     Raises:
-        HTTPException: Returns a 404 error if no client is found with the provided attributes.
+        HTTPException: Returns a 404 error if no client is found with the 
+        provided attributes.
     """
     client = get_client_data(age, gender, work_experience)
     if client:
@@ -90,25 +98,29 @@ async def get_client(age: int, gender: int, work_experience: int):
     else:
         raise HTTPException(status_code=404, detail="Client not found")
 
+
 @router.put("/", response_model=ClientData)
 async def update_client(client_update: ClientData):
     """
     Update an existing client's data.
 
     Args:
-        client_update (ClientData): A model containing the client's updated data.
+        client_update (ClientData): A model containing the client's updated 
+        data.
 
     Returns:
         ClientData: The updated client data.
 
     Raises:
-        HTTPException: Returns a 404 error if no client is found with the provided attributes, or if the update fails.
+        HTTPException: Returns a 404 error if no client is found with the 
+        provided attributes, or if the update fails.
     """
     updated_client = update_client_data(client_update.dict())
     if updated_client:
         return updated_client
     else:
         raise HTTPException(status_code=404, detail="Unable to update client")
+
 
 @router.delete("/", response_model=dict)
 async def delete_client(age: int, gender: int, work_experience: int):
@@ -124,7 +136,8 @@ async def delete_client(age: int, gender: int, work_experience: int):
         dict: A message indicating successful deletion.
 
     Raises:
-        HTTPException: Returns a 404 error if no client is found with the provided attributes, or if the deletion fails.
+        HTTPException: Returns a 404 error if no client is found with the 
+        provided attributes, or if the deletion fails.
     """
     if delete_client_data(age, gender, work_experience):
         return {"message": "Client deleted successfully"}
